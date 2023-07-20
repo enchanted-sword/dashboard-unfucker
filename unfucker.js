14efaad6ccfbb39cbb3083193e822dd35bc5ccc5
Marcus <marcustyphoon@gmail.com>
2023-07-20 11:18:17 -0700

    Revert "Replace class names with CSS map utilities"
    This reverts commit c58019a90be01135dfc3c9068921ce8a62eeb7a9.

--- unfucker.js
+++ unfucker.js
@@ -15,24 +15,24 @@
 var $ = window.jQuery;
 
 function $unfuck ({ keyToClasses, keyToCss }) {
-    if ($(keyToCss("headerWrapper")).length) {
+    if ($(".hlDot").length) {
         console.log("no need to unfuck");
         return
     }
-    var $aside = $(keyToCss("sidebar")).eq(0);
-    var $search = $aside.find(keyToCss("searchSidebarItem")).eq(0);
-    var $nav = $(keyToCss("navigationLinks")).eq(0);
-    var $content = $(keyToCss("main")).eq(0);
-    var $main = $(keyToCss("newDesktopLayout")).eq(0);
-    var $bar = $("<div>", {class: keyToClasses("headerWrapper")[0]});
-    var $logo = $(keyToCss("logoContainer")).eq(0);
-    var $footer = $(keyToCss("usesNewDimensions")).eq(0);
-    var $create = $(keyToCss("createPost")).eq(0);
+    var $aside = $(".e1knl").eq(0);
+    var $search = $aside.find(".N5wJr").eq(0);
+    var $nav = $(".gM9qK").eq(0);
+    var $content = $(".lSyOz").eq(0);
+    var $main = $(".ZkG01").eq(0);
+    var $bar = $("<div>", {class: "hlDot"});
+    var $logo = $(".Gav7q").eq(0);
+    var $footer = $(".j_AvJ").eq(0);
+    var $create = $(".jGgIg").eq(0);
     $aside.detach();
     $search.detach();
     $create.detach();
     $bar.css({display: "flex"});
-    $(keyToCss("bluespaceLayout")).prepend($bar);
+    $(".D5eCV").prepend($bar);
     $logo.detach()
     $bar.append($logo);
     $bar.append($search);
@@ -42,24 +42,24 @@ function $unfuck ({ keyToClasses, keyToCss }) {
     $main.append($aside)
     $logo.css("margin-left", "100px");
     $search.css("max-width", "550px");
-    $(keyToCss("mainContentWrapper")).eq(0).remove();
-    $(keyToCss("navigationWrapper")).eq(0).remove();
-    $(keyToCss("navigation")).css({border: "none"});
+    $("._3xgk").eq(0).remove();
+    $(".h_Erh").eq(0).remove();
+    $(".FA5JM").css({border: "none"});
     $main.css({border: "none", marginTop: "40px"});
     $aside.css({marginLeft: "50px"})
     $aside.children().eq(0).css({position: "fixed", width: "320px"});
-    $(`${keyToCss("about")}${keyToCss("inSidebar")}${keyToCss("usesNewDimensions")}`).css("position", "fixed");
-    $(`${keyToCss("post")} ${keyToCss("stickyContainer")} ${keyToCss("avatar")}${keyToCss("newDesktopLayout")}`).css("top", "calc(70px + var(--dashboard-tabs-header-height,0px))");
-    $(keyToCss("searchShadow")).css("background", "none");
+    $(".m8mN_._qHCt.j_AvJ").css("position", "fixed");
+    $(".FtjPK .AD_w7 .JZ10N.y0ud2").css("top", "calc(70px + var(--dashboard-tabs-header-height,0px))");
+    $(".zmjaW").css("background", "none");
     $nav.css({display: "flex", justifyContent: "flex-end", flexBasis: "100%", marginBottom: "0px", marginTop: "8px"});
     $nav.children(":nth-child(2),:nth-child(10),:nth-child(11),:nth-child(12)").remove();
     $nav.find("svg").css("scale", "1.4");
-    $(`${keyToCss("startChildWrapper")} + ${keyToCss("navInfo")}`).remove();
-    $(keyToCss("subNav")).css({height: "800px", overflowY: "scroll", background: "RGB(var(--white))", scrollbarColor: "rgba(var(--black),.4)rgba(var(--white),.1)", color: "RGB(var(--black))", position: "absolute", borderRadius: "4px", marginTop: "48px"});
-    $(`${keyToCss("subNav")} *`).css({color: "RGB(var(--black))"});
-    $(`${keyToCss("subNav")} li`).css({listStyleType: "none", borderBottom: "rgba(var(--black),.07)"});
+    $(".kn4U3 + .a132D").remove();
+    $(".jL4Qq").css({height: "800px", overflowY: "scroll", background: "RGB(var(--white))", scrollbarColor: "rgba(var(--black),.4)rgba(var(--white),.1)", color: "RGB(var(--black))", position: "absolute", borderRadius: "4px", marginTop: "48px"});
+    $(".jL4Qq *").css({color: "RGB(var(--black))"});
+    $(".jL4Qq li").css({listStyleType: "none", borderBottom: "rgba(var(--black),.07)"});
     $("#settings_subnav").css("height", "fit-content");
-    $(keyToCss("navSubHeader")).replaceWith(`<div class="${keyToClasses("heading")[0]}" style="display: flex;align-items: center;justify-content: space-between;background: rgba(var(--black),.07);height: 36px;padding: 4px 12px 4px 12px;color: rgba(var(--black),.65);"><h3>Blogs</h3><a class="${keyToClasses("headingLink")[0]}" style="text-decoration: none;" href="/new/blog">+ New</a></div>`);
+    $(".h8SQv").replaceWith(`<div class="l463r" style="display: flex;align-items: center;justify-content: space-between;background: rgba(var(--black),.07);height: 36px;padding: 4px 12px 4px 12px;color: rgba(var(--black),.65);"><h3>Blogs</h3><a class="Oyvhq" style="text-decoration: none;" href="/new/blog">+ New</a></div>`);
     $footer.css({position: "fixed", height: "20px", bottom: "0px", right: "50px"});
     $create.css({width: "44px", marginRight: "100px", marginLeft: "10px"});
     $create.children().eq(0).css({borderRadius: "3px"});
