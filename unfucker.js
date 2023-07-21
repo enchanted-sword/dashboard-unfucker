e8e09ce3e385d9dd8569ad044025e8b440b4670a
April <81981111+enchanted-sword@users.noreply.github.com>
2023-07-21 00:22:22 -0300

    switch navbar tweaks to using attribute selectors, hide tumblr radar

--- unfucker.js
+++ unfucker.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         dashboard unfucker
-// @version      1.6
+// @version      1.7
 // @description  no more shitty twitter ui for pc
 // @author       dragongirlsnout
 // @match        https://www.tumblr.com/*
@@ -15,56 +15,58 @@
 
 getCssMapUtilities().then(({ keyToClasses, keyToCss }) => {
 
-var $ = window.jQuery;
+    var $ = window.jQuery;
 
-async function $unfuck () {
-    if ($("#__hw").length) {
-        console.log("no need to unfuck");
-        return
-    }
-    else {console.log("unfucking dashboard...")}
-    if (!$(keyToCss("navigationLinks")).length) {
-        console.log("page not loaded, retrying...");
-        throw "page not loaded";
-    }
-    var match = ["", "dashboard", "settings", "blog", "domains", "search", "likes", "following", "inbox", "tagged"]
-    var compare = window.location.href.split("/")[3].split("?")[0];
-    var test = true;
-    for (let x in match) {
-        if (compare === match[x]) {
-            test = true;
-            break;
+    async function $unfuck () {
+        if ($("#__hw").length) {
+            console.log("no need to unfuck");
+            return
+        }
+        else {console.log("unfucking dashboard...")}
+        if (!$(keyToCss("navigationLinks")).length) {
+            console.log("page not loaded, retrying...");
+            throw "page not loaded";
+        }
+        var match = ["", "dashboard", "settings", "blog", "domains", "search", "likes", "following", "inbox", "tagged"]
+        var compare = window.location.href.split("/")[3].split("?")[0];
+        var test = true;
+        for (let x in match) {
+            if (compare === match[x]) {
+                test = true;
+                break;
+            }
+            else {
+                test = false;
+                continue;
+            }
+        }
+        var $nav = $(keyToCss("navigationLinks")).eq(0);
+        var $content = ""
+        var $main = $(keyToCss("newDesktopLayout")).eq(0);
+        var $bar = $("<div>", {class: keyToClasses("headerWrapper").join(" "), id: "__hw"});
+        var $logo = $(keyToCss("logoContainer")).eq(0);
+        var $create = $(keyToCss("createPost")).eq(0);
+        var $search = ""
+        $create.detach();
+        $bar.css({display: "flex"});
+        $(keyToCss("bluespaceLayout")).prepend($bar);
+        $logo.detach()
+        $bar.append($logo);
+        if (test) {
+            var $aside = $(keyToCss("sidebar")).eq(0);
+            $search = $(keyToCss("searchSidebarItem")).eq(0);
+            $content = $(keyToCss("main")).eq(0);
+            $aside.detach();
+            $main.append($aside)
+            $aside.css({marginLeft: "50px", position: "sticky", top: "54px", height: "fit-content"})
+            $aside.children().eq(0).css({width: "320px"});
+            $(`${keyToCss("about")}${keyToCss("inSidebar")}${keyToCss("usesNewDimensions")}`).css({position: "fixed", height: "20px", bottom: "0px"});
+            $(keyToCss("sidebarItem")).remove();
+            $(keyToCss("tabsHeader")).remove();
         }
         else {
-            test = false;
-            continue;
-        }
-    }
-    var $nav = $(keyToCss("navigationLinks")).eq(0);
-    var $content = ""
-    var $main = $(keyToCss("newDesktopLayout")).eq(0);
-    var $bar = $("<div>", {class: keyToClasses("headerWrapper").join(" "), id: "__hw"});
-    var $logo = $(keyToCss("logoContainer")).eq(0);
-    var $create = $(keyToCss("createPost")).eq(0);
-    var $search = ""
-    $create.detach();
-    $bar.css({display: "flex"});
-    $(keyToCss("bluespaceLayout")).prepend($bar);
-    $logo.detach()
-    $bar.append($logo);
-    if (test) {
-        var $aside = $(keyToCss("sidebar")).eq(0);
-        $search = $(keyToCss("searchSidebarItem")).eq(0);
-        $content = $(keyToCss("main")).eq(0);
-        $aside.detach();
-        $main.append($aside)
-        $aside.css({marginLeft: "50px", position: "sticky", top: "54px", height: "fit-content"})
-        $aside.children().eq(0).css({width: "320px"});
-        $(`${keyToCss("about")}${keyToCss("inSidebar")}${keyToCss("usesNewDimensions")}`).css({position: "fixed", height: "20px", bottom: "0px"});
-    }
-    else {
-        $content = $(`${keyToCss("mainContentWrapper")} ${keyToCss("container")}`).eq(0);
-        $search = $(`
+            $content = $(`${keyToCss("mainContentWrapper")} ${keyToCss("container")}`).eq(0);
+            $search = $(`
             <div class="${keyToClasses("searchSidebarItem").join(" ")}" style="max-width: 550px; width: 100%; padding: 14px 8px 0px 8px" >
                 <div class="${keyToClasses("formContainer").join(" ")}">
                 <span data-testid="controlled-popover-wrapper" class="${keyToClasses("targetWrapper").join(" ")}">
@@ -93,57 +95,59 @@ async function $unfuck () {
                 </div>
             </div>
         `)
+        }
+        if (compare === "search" || compare === "tagged") {
+            $content.css("max-width", "fit-content");
+        }
+        $search.detach();
+        $bar.append($search);
+        $bar.append($nav);
+        $bar.append($create);
+        $content.detach();
+        $main.prepend($content)
+        $logo.css("margin-left", "100px");
+        $search.css("max-width", "550px");
+        $(keyToCss("mainContentWrapper")).eq(0).remove();
+        $(keyToCss("navigationWrapper")).eq(0).remove();
+        $(keyToCss("navigation")).css({border: "none"});
+        $main.css({border: "none", marginTop: "40px"});
+        $(`${keyToCss("post")} ${keyToCss("stickyContainer")} ${keyToCss("avatar")}${keyToCss("newDesktopLayout")}`).css("top", "calc(70px + var(--dashboard-tabs-header-height,0px))");
+        $(keyToCss("searchShadow")).css("background", "none");
+        $nav.css({display: "flex", justifyContent: "flex-end", flexBasis: "100%", marginBottom: "0px", marginTop: "8px"});
+        $nav.children().has('use[href="#managed-icon__explore"]')
+            .add($nav.children().has('use[href="#managed-icon__shop"]'))
+            .add($nav.children().has('use[href="#managed-icon__live-video"]'))
+            .add($nav.children().has('use[href="#managed-icon__earth"]'))
+            .add($nav.children().has('use[href="#managed-icon__sparkle"]'))
+            .remove();
+        if ($(window).width() < 1150) {
+            $(keyToCss("buttonInner")).css("padding", "8px 16px");
+        }
+        $nav.find("svg").css("scale", "1.4");
+        $(`${keyToCss("startChildWrapper")} + ${keyToCss("navInfo")}`).remove();
+        $(keyToCss("blogTile")).css("list-style-type", "none");
+        $(keyToCss("subNav")).css({height: "800px", overflowY: "scroll", background: "RGB(var(--white))", scrollbarColor: "rgba(var(--black),.4)rgba(var(--white),.1)", color: "RGB(var(--black))", position: "absolute", borderRadius: "4px", marginTop: "48px"});
+        $(`${keyToCss("subNav")} *`).css({color: "RGB(var(--black))"});
+        var $navli = $(`${keyToCss("subNav")} > ${keyToCss("navItem")}, ${keyToCss("accountStats")} li`);
+        $navli.css({listStyleType: "none", borderBottom: "1px solid rgba(var(--black),.07)"});
+        $navli.on("mouseenter", function() {$(this).css("background-color", "rgba(var(--black),.07)")});
+        $navli.on("mouseleave", function() {$(this).css("background-color", "rgb(var(--white))")});
+        $("#managed-icon__caret-thin").css("--icon-color-primary", "rgba(var(--black),.65)")
+        $("#settings_subnav").css("height", "fit-content");
+        $(keyToCss("navSubHeader")).replaceWith(`<div class="${keyToClasses("heading").join(" ")}" style="display: flex;align-items: center;justify-content: space-between;background: rgba(var(--black),.07);height: 36px;padding: 4px 12px 4px 12px;color: rgba(var(--black),.65);"><h3>Blogs</h3><a class="${keyToClasses("headingLink").join(" ")}" style="text-decoration: none;" href="/new/blog">+ New</a></div>`);
+        $create.css({width: "44px", marginRight: "100px", marginLeft: "10px"});
+        $create.children().eq(0).css({borderRadius: "3px"});
+        $create.find("a").eq(0).html('<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" role="presentation"><use href="#managed-icon__post"></use></svg>');
+        console.log("dashboard fixed!");
     }
-    if (compare === "search" || compare === "tagged") {
-        $content.css("max-width", "fit-content");
-    }
-    $search.detach();
-    $bar.append($search);
-    $bar.append($nav);
-    $bar.append($create);
-    $content.detach();
-    $main.prepend($content)
-    $logo.css("margin-left", "100px");
-    $search.css("max-width", "550px");
-    $(keyToCss("mainContentWrapper")).eq(0).remove();
-    $(keyToCss("navigationWrapper")).eq(0).remove();
-    $(keyToCss("navigation")).css({border: "none"});
-    $main.css({border: "none", marginTop: "40px"});
-    $(`${keyToCss("post")} ${keyToCss("stickyContainer")} ${keyToCss("avatar")}${keyToCss("newDesktopLayout")}`).css("top", "calc(70px + var(--dashboard-tabs-header-height,0px))");
-    $(keyToCss("searchShadow")).css("background", "none");
-    $nav.css({display: "flex", justifyContent: "flex-end", flexBasis: "100%", marginBottom: "0px", marginTop: "8px"});
-    if ($(window).width() >= 1150) {
-        $nav.children(":nth-child(2),:nth-child(10),:nth-child(11),:nth-child(12)").remove();
-    }
-    else {
-        $nav.children(":nth-child(2),:nth-child(8),:nth-child(9),:nth-child(10)").remove();
-        $(keyToCss("buttonInner")).css("padding", "8px 16px");
-    }
-    $nav.find("svg").css("scale", "1.4");
-    $(`${keyToCss("startChildWrapper")} + ${keyToCss("navInfo")}`).remove();
-    $(keyToCss("blogTile")).css("list-style-type", "none");
-    $(keyToCss("subNav")).css({height: "800px", overflowY: "scroll", background: "RGB(var(--white))", scrollbarColor: "rgba(var(--black),.4)rgba(var(--white),.1)", color: "RGB(var(--black))", position: "absolute", borderRadius: "4px", marginTop: "48px"});
-    $(`${keyToCss("subNav")} *`).css({color: "RGB(var(--black))"});
-    var $navli = $(`${keyToCss("subNav")} > ${keyToCss("navItem")}, ${keyToCss("accountStats")} li`);
-    $navli.css({listStyleType: "none", borderBottom: "1px solid rgba(var(--black),.07)"});
-    $navli.on("mouseenter", function() {$(this).css("background-color", "rgba(var(--black),.07)")});
-    $navli.on("mouseleave", function() {$(this).css("background-color", "rgb(var(--white))")});
-    $("#managed-icon__caret-thin").css("--icon-color-primary", "rgba(var(--black),.65)")
-    $("#settings_subnav").css("height", "fit-content");
-    $(keyToCss("navSubHeader")).replaceWith(`<div class="${keyToClasses("heading").join(" ")}" style="display: flex;align-items: center;justify-content: space-between;background: rgba(var(--black),.07);height: 36px;padding: 4px 12px 4px 12px;color: rgba(var(--black),.65);"><h3>Blogs</h3><a class="${keyToClasses("headingLink").join(" ")}" style="text-decoration: none;" href="/new/blog">+ New</a></div>`);
-    $create.css({width: "44px", marginRight: "100px", marginLeft: "10px"});
-    $create.children().eq(0).css({borderRadius: "3px"});
-    $create.find("a").eq(0).html('<svg xmlns="http://www.w3.org/2000/svg" height="24" width="24" role="presentation"><use href="#managed-icon__post"></use></svg>');
-    console.log("dashboard fixed!");
-}
 
-$unfuck();
+    $unfuck();
 
-window.tumblr.on('navigation', () => requestAnimationFrame(function() {
-    $unfuck().catch((e) => {
-        window.setTimeout($unfuck, 400)
-    });
-}));
+    window.tumblr.on('navigation', () => requestAnimationFrame(function() {
+        $unfuck().catch((e) => {
+            window.setTimeout($unfuck, 400)
+        });
+    }));
 
 });
 
