f5695cf964d8a74cf3926a8805a509b7035abc11
April <81981111+enchanted-sword@users.noreply.github.com>
2023-08-13 10:53:58 -0300

    update redirect

--- unfucker.js
+++ unfucker.js
@@ -1,1 +1,1 @@
-//file was changed to unfucker.user.js, navigate to that file and click on the raw button with tampermonkey installed to install the script
+//file was changed to https://raw.githubusercontent.com/enchanted-sword/dashboard-unfucker/main/unfucker.user.js, navigate to that file with tampermonkey installed to install the script
