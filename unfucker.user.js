3717acc5a9900af42def9c62ca66556af44965bf
April <81981111+enchanted-sword@users.noreply.github.com>
2023-08-17 16:25:58 -0300

    update version

--- unfucker.user.js
+++ unfucker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         dashboard unfucker
-// @version      3.3.0
+// @version      3.4.0
 // @description  no more shitty twitter ui for pc
 // @author       dragongirlsnout
 // @match        https://www.tumblr.com/*
