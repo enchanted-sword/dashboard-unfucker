1db60d2567ab5ff7ba16abde0e4608f462a285fb
enchanted-sword <xlucas4956@gmail.com>
2023-12-02 16:14:04 -0400

    fix version

--- unfucker.user.js
+++ unfucker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         dashboard unfucker
-// @version      5.4.5.1
+// @version      5.4.6
 // @description  no more shitty twitter ui for pc
 // @author       dragongirlsnout
 // @match        https://www.tumblr.com/*
@@ -17,7 +17,7 @@
 'use strict';
 var $ = window.jQuery;
 const main = async function () {
-  const version = "5.4.5";
+  const version = "5.4.6";
   const match = [
     "",
     "dashboard",
